909d8701b74ac757e4722a51f29ffecdcda4bca2
Somya Kajla <kajlasomya@seattleu.edu>
2020-01-31 00:01:32 -0800

    Milestone3: Implementation Of Create_table, Drop_table, Show_tables, Show_columns

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -3,6 +3,7 @@
  * @author Kevin Lundeen
  * @see "Seattle University, CPSC5300, Summer 2018"
  */
+
 #include "SQLExec.h"
 using namespace std;
 using namespace hsql;
@@ -41,13 +42,29 @@ ostream &operator<<(ostream &out, const QueryResult &qres) {
     return out;
 }
 
+/*
+ * Destructor: clean up all the query result data from all the rows
+ * after printing the rows.
+ */ 
 QueryResult::~QueryResult() {
-	// FIXME
+    if (column_names != NULL)
+        delete column_names;
+    if (column_attributes != NULL)
+        delete column_attributes;
+    if (rows != NULL)
+        for(ValueDict *row: *rows)
+            delete row;
+    delete rows;
 }
 
-
+/*
+ * This method exceute all the query of the basis of statement type.
+ * Currently Support : Create, Drop, Show (Table)
+ */
 QueryResult *SQLExec::execute(const SQLStatement *statement) throw(SQLExecError) {
-    // FIXME: initialize _tables table, if not yet present
+    // Initialize static system table object to be used through out the execution of query.
+    if (tables == NULL) 
+        SQLExec::tables = new Tables();
 
     try {
         switch (statement->type()) {
@@ -65,29 +82,220 @@ QueryResult *SQLExec::execute(const SQLStatement *statement) throw(SQLExecError)
     }
 }
 
+/*
+ * Duplicate to getColumnAttributes Not getting used in my implementation.
+ */
 void SQLExec::column_definition(const ColumnDefinition *col, Identifier& column_name,
-                                ColumnAttribute& column_attribute) {
-	throw SQLExecError("not implemented");  // FIXME
+        ColumnAttribute& column_attribute) {
+    throw SQLExecError("not implemented");  // FIXME
 }
 
+/* 
+ * Provided ColumnAttribute on the basis col definition privided by 
+ * statement in create_table method.
+ */
+ColumnAttribute getColumnAttributes(ColumnDefinition *col){
+    ColumnAttribute *ct = new ColumnAttribute();
+    switch (col->type) {
+        case ColumnDefinition::INT:
+            ct->set_data_type(ColumnAttribute::INT);
+            break;
+        case ColumnDefinition::TEXT:
+            ct->set_data_type(ColumnAttribute::TEXT);
+            break;
+        default:
+            throw SQLExecError("unrecognized data type");
+    }
+    return *ct;
+}
+
+/*
+ * Create Statement to Create SQL Objects Table.
+ */
 QueryResult *SQLExec::create(const CreateStatement *statement) {
-	return new QueryResult("not implemented"); // FIXME
+    switch(statement->type) {
+        case CreateStatement::kTable:
+            return create_table(statement);
+        default:
+            return new QueryResult("Only CREATE TABLE are implemented");
+    }
 }
 
-// DROP ...
+/*
+ * Create table:
+ * 1. get all the columns from statement and convert column data type into column attributes.
+ * 2. insert table name into _tables schema system table 
+ * 3. insert entry into _columns schema table.
+ * 4. create berkley db file of the table.
+ * 5. if any exceptions happens remove all the respective entry from all the schema tables.
+ */
+QueryResult *SQLExec::create_table(const CreateStatement *statement) {
+    Identifier table_name = statement->tableName;
+    ColumnNames columnNames;
+    ColumnAttributes columnAttributes;
+
+    // get all the columns and column attributes.
+    for(ColumnDefinition *col : *statement->columns){
+        columnNames.push_back(col->name);
+        columnAttributes.push_back(getColumnAttributes(col));
+    }
+
+    // insert values into _table schema 
+    ValueDict row;
+    row["table_name"] = Value(table_name);
+    Handle t_handle = tables->insert(&row);
+
+    // insert values into _columns schema table
+    try {
+        Handles col_handles;  // this handle will be used to delete added columns if any exception happens ie, duplicate column case
+        DbRelation &column_table = tables->get_table(Columns::TABLE_NAME);
+        try {
+            for(uint i=0; i< columnNames.size(); i++){
+                row["column_name"] = Value(columnNames[i]);
+                row["data_type"] = Value(columnAttributes[i].get_data_type() == ColumnAttribute::INT ? "INT" : "TEXT");
+                col_handles.push_back(column_table.insert(&row)); // storing handles into col_handles to remove & adding _columns 
+            }
+
+            // Create Table Object and then create its physical  berkley db file.
+            DbRelation& table = SQLExec::tables->get_table(table_name);
+            if (statement->ifNotExists)
+                table.create_if_not_exists();
+            else
+                table.create();
+        } catch(exception& e){
+            // remove from _columns if exception occurs 
+            try {
+                for(auto const &handle : col_handles)
+                    column_table.del(handle);
+            } catch(exception& e) { }
+            throw;
+        } 
+    }catch(exception& e) {
+        try{
+            SQLExec::tables->del(t_handle);  // remove entry from _tables,
+        } catch(exception& e) { }
+        throw;
+    }
+
+    return new QueryResult("created " + table_name);
+}
+
+/*
+ * calls the respective drop method on basis for drop command.
+ */
 QueryResult *SQLExec::drop(const DropStatement *statement) {
-	return new QueryResult("not implemented"); // FIXME
+    switch(statement->type) {
+        case DropStatement::kTable:
+            return drop_table(statement);
+        default:
+            return new QueryResult("Only DROP TABLE are implemented");
+    }
 }
 
+/*
+ * Drop Table: drops the table.
+ * 1. should not drop schema tables.
+ * 2. delete entry from _column table and then from _table.
+ * 3. delete berkley db file.
+ */
+QueryResult *SQLExec::drop_table(const DropStatement *statement) {
+    Identifier table_name = statement->name;
+    if(table_name == Tables::TABLE_NAME || table_name == Columns::TABLE_NAME) {
+        return new QueryResult("you can't delete shema tables"); // FIXME
+    } 
+
+    DbRelation& table = SQLExec::tables->get_table(table_name);
+
+    // delete entries from _columns tables. 
+    ValueDict where;
+    where["table_name"] = Value(table_name);
+
+    // get _column table object.
+    DbRelation& _column = tables->get_table(Columns::TABLE_NAME);
+
+    Handles* handles = _column.select(&where);
+    for(Handle handle : *handles){
+        _column.del(handle);
+    }
+    delete handles;
+
+    // Delete physical berkley db file.
+    table.drop();
+
+    // delete entries from _tables tables. deletes entry from table cache as well.
+    handles = tables->select(&where);
+    for(Handle handle : *handles){
+        tables->del(handle);
+    }
+
+    delete handles;
+    return new QueryResult(string("dropped ") + table_name);
+}
+
+/* 
+ * Show entries from _tables / _columns
+ * Support ( show tables, show columns)
+ */
 QueryResult *SQLExec::show(const ShowStatement *statement) {
-	return new QueryResult("not implemented"); // FIXME
+    switch(statement->type){
+        case ShowStatement::kTables:
+            return show_tables();
+        case ShowStatement::kColumns:
+            return show_columns(statement);
+        default:
+            return new QueryResult("not implemented");
+            //throw SQLExceError("unrecognised SHOW type");
+    } 
+    return NULL; 
 }
 
+/*
+ * show tables : show entries of _tables schema system table.
+ * 1. get all the columns and column attributes of _tables table.
+ * 2. get all the entries from _table and only show table name which are not _tables or _columns
+ */
 QueryResult *SQLExec::show_tables() {
-	return new QueryResult("not implemented"); // FIXME
+    ColumnNames *resultsColNames = new ColumnNames();
+    ColumnAttributes *resultsColAttribs = new ColumnAttributes();
+
+    // get column names and column attributes of _tables
+    tables->get_columns(Tables::TABLE_NAME, *resultsColNames, *resultsColAttribs);
+
+    Handles *handles = tables->select();  // get handles of all the tables entries from tables.
+    ValueDicts *rows = new ValueDicts();
+    for(Handle handle : *handles) {
+        ValueDict *row = tables->project(handle, resultsColNames);
+        if(row->at("table_name") != Value("_tables") && row->at("table_name") != Value("_columns")) 
+            rows->push_back(row);
+    }
+
+    delete handles;
+    return new QueryResult(resultsColNames, resultsColAttribs, rows,"successfully returned " + to_string(rows->size()) + " rows");
 }
 
+/*
+ * show columns: show entries from _columns table.
+ * 1. get all the columns and column attributes of _columns table.
+ * 2. get _column table object and shows all the entries supporting where clause.
+ */
 QueryResult *SQLExec::show_columns(const ShowStatement *statement) {
-	return new QueryResult("not implemented"); // FIXME
-}
+    ColumnNames *resultsColNames = new ColumnNames();
+    ColumnAttributes *resultsColAttribs = new ColumnAttributes();
 
+    tables->get_columns(Columns::TABLE_NAME, *resultsColNames, *resultsColAttribs);
+
+    ValueDict where;
+    where["table_name"] = Value(statement->tableName);
+
+    DbRelation &column_table = tables->get_table(Columns::TABLE_NAME);
+    Handles *handles = column_table.select(&where); // get all the handles supporting where clause.
+
+    ValueDicts *rows = new ValueDicts;
+    for(Handle handle : *handles) {
+        ValueDict *row = column_table.project(handle, resultsColNames);
+        rows->push_back(row);
+    }
+
+    delete handles;
+    return new QueryResult(resultsColNames, resultsColAttribs, rows,"successfully returned " + to_string(rows->size()) + " rows");
+}
--- SQLExec.h
+++ SQLExec.h
@@ -14,8 +14,8 @@
  * @class SQLExecError - exception for SQLExec methods
  */
 class SQLExecError : public std::runtime_error {
-public:
-    explicit SQLExecError(std::string s) : runtime_error(s) {}
+    public:
+        explicit SQLExecError(std::string s) : runtime_error(s) {}
 };
 
 
@@ -23,28 +23,28 @@ public:
  * @class QueryResult - data structure to hold all the returned data for a query execution
  */
 class QueryResult {
-public:
-    QueryResult() : column_names(nullptr), column_attributes(nullptr), rows(nullptr), message("") {}
+    public:
+        QueryResult() : column_names(nullptr), column_attributes(nullptr), rows(nullptr), message("") {}
 
-    QueryResult(std::string message) : column_names(nullptr), column_attributes(nullptr), rows(nullptr),
-                                       message(message) {}
+        QueryResult(std::string message) : column_names(nullptr), column_attributes(nullptr), rows(nullptr),
+        message(message) {}
 
-    QueryResult(ColumnNames *column_names, ColumnAttributes *column_attributes, ValueDicts *rows, std::string message)
+        QueryResult(ColumnNames *column_names, ColumnAttributes *column_attributes, ValueDicts *rows, std::string message)
             : column_names(column_names), column_attributes(column_attributes), rows(rows), message(message) {}
 
-    virtual ~QueryResult();
+        virtual ~QueryResult();
 
-    ColumnNames *get_column_names() const { return column_names; }
-    ColumnAttributes *get_column_attributes() const { return column_attributes; }
-    ValueDicts *get_rows() const { return rows; }
-    const std::string &get_message() const { return message; }
-    friend std::ostream &operator<<(std::ostream &stream, const QueryResult &qres);
+        ColumnNames *get_column_names() const { return column_names; }
+        ColumnAttributes *get_column_attributes() const { return column_attributes; }
+        ValueDicts *get_rows() const { return rows; }
+        const std::string &get_message() const { return message; }
+        friend std::ostream &operator<<(std::ostream &stream, const QueryResult &qres);
 
-protected:
-    ColumnNames *column_names;
-    ColumnAttributes *column_attributes;
-    ValueDicts *rows;
-    std::string message;
+    protected:
+        ColumnNames *column_names;
+        ColumnAttributes *column_attributes;
+        ValueDicts *rows;
+        std::string message;
 };
 
 
@@ -52,31 +52,33 @@ protected:
  * @class SQLExec - execution engine
  */
 class SQLExec {
-public:
-	/**
-	 * Execute the given SQL statement.
-	 * @param statement   the Hyrise AST of the SQL statement to execute
-	 * @returns           the query result (freed by caller)
-	 */
-    static QueryResult *execute(const hsql::SQLStatement *statement) throw(SQLExecError);
+    public:
+        /**
+         * Execute the given SQL statement.
+         * @param statement   the Hyrise AST of the SQL statement to execute
+         * @returns           the query result (freed by caller)
+         */
+        static QueryResult *execute(const hsql::SQLStatement *statement) throw(SQLExecError);
 
-protected:
-	// the one place in the system that holds the _tables table
-    static Tables *tables;
+    protected:
+        // the one place in the system that holds the _tables table
+        static Tables *tables;
 
-	// recursive decent into the AST
-    static QueryResult *create(const hsql::CreateStatement *statement);
-    static QueryResult *drop(const hsql::DropStatement *statement);
-    static QueryResult *show(const hsql::ShowStatement *statement);
-    static QueryResult *show_tables();
-    static QueryResult *show_columns(const hsql::ShowStatement *statement);
+        // recursive decent into the AST
+        static QueryResult *create(const hsql::CreateStatement *statement);
+        static QueryResult *create_table(const hsql::CreateStatement *statement);
+        static QueryResult *drop(const hsql::DropStatement *statement);
+        static QueryResult *drop_table(const hsql::DropStatement *statement);
+        static QueryResult *show(const hsql::ShowStatement *statement);
+        static QueryResult *show_tables();
+        static QueryResult *show_columns(const hsql::ShowStatement *statement);
 
-	/**
-	 * Pull out column name and attributes from AST's column definition clause
-	 * @param col                AST column definition
-	 * @param column_name        returned by reference
-	 * @param column_attributes  returned by reference
-	 */
-    static void column_definition(const hsql::ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute);
+        /**
+         * Pull out column name and attributes from AST's column definition clause
+         * @param col                AST column definition
+         * @param column_name        returned by reference
+         * @param column_attributes  returned by reference
+         */
+        static void column_definition(const hsql::ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute);
 };
 
